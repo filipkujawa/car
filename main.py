be2d6a5454617397b4048cbd09e68234ea7bcc75
filip <44206832+filipkujawa@users.noreply.github.com>
2022-02-14 16:51:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import explorerhat
+from time import sleep
+from bluedot import BlueDot
+from signal import pause
+
+
+
+speed = 100
+
+def up():
+    explorerhat.motor.one.forward(speed - 10)
+    explorerhat.motor.two.forward(speed)
+def down():
+    explorerhat.motor.one.backward(speed)
+    explorerhat.motor.two.backward(speed)
+def left():
+    explorerhat.motor.one.forward(speed / 2)
+    explorerhat.motor.two.forward(speed)
+def right():
+    explorerhat.motor.one.forward(speed)
+    explorerhat.motor.two.forward(speed / 2)
+        
+def off():
+    explorerhat.motor.one.stop()
+    explorerhat.motor.two.stop()
+    
+
+bd = BlueDot(cols=3, rows=3)
+bd.color = "gray"
+bd.square = True
+bd.border = True
+
+bd[0,0].visible = False
+bd[2,0].visible = False
+bd[0,2].visible = False
+bd[2,2].visible = False
+bd[1,1].visible = True
+
+
+
+bd[1,0].when_pressed = up
+bd[1,2].when_pressed = down
+bd[0,1].when_pressed = left
+bd[2,1].when_pressed = right
+bd[1,1].when_pressed = off
+
+pause()
